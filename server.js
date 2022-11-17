77a627a43c36c1e00ee0db395499bc6d4eaf16c2
menamilochomil <jimenamilochomil@gmail.com>
2022-11-17 14:00:47 -0600

    comment: adds info to test the auth in server.js

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log('JSON Server is running')
 })
+
+
+//Aquí comienzan la prueba de auth link: https://www.npmjs.com/package/json-server-auth
+
+// const jsonServer = require('json-server')
+// const auth = require('json-server-auth')
+
+// const app = jsonServer.create()
+// const router = jsonServer.router('db.json')
+
+// // /!\ Bind the router db to the app
+// app.db = router.db
+
+// // You must apply the auth middleware before the router
+// app.use(auth)
+// app.use(router)
+// app.listen(3000)
